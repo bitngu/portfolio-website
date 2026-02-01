f54da55d7e01a0f3bbf28b7bd5d75e03855a575c
bitngu <bitngu3@gmail.com>
2026-02-01 14:10:28 -0800

    feat: add gallery view to about me

--- src/Components/WindowTitleBar.tsx
+++ src/Components/WindowTitleBar.tsx
@@ -1,5 +1,6 @@
 import { appService, type TaskbarData } from '../classes/AppData'
 import '../styles/WindowTitleBar.scss'
+import {type MouseEvent } from 'react'
 
 
 export const WindowTitleBar = (props: TaskbarData) => {
@@ -12,23 +13,34 @@ export const WindowTitleBar = (props: TaskbarData) => {
 
     }
 
-    const closeWindow = () => {
+    const closeWindow = (event: MouseEvent<HTMLButtonElement>) => {
+        event.stopPropagation();
         appService.removeActiveApp(props);
     }
 
     return (
-        <div className="title-bar">
-            <div className="title-bar--content">
-                <div className="icon">
-                    <img src={props.icon}/>
+        <div className='title-bar-wrapper'>
+            <div className="title-bar">
+                <div className="title-bar--content">
+                    <div className="icon">
+                        <img src={props.icon}/>
+                    </div>
+                    <span>{props.displayName}</span>
                 </div>
-                <span>{props.displayName}</span>
+                <div className='title-bar--controls'>
+                    <button type='button' className='minimize' onClick={minimizeWindow}></button>
+                    <button type='button' className='expand' onClick={expandWindow}></button>
+                    <button type='button' className='close' onClick={event => closeWindow(event)}></button>
+                </div>
             </div>
-            <div className='title-bar--controls'>
-                <button type='button' className='minimize' onClick={minimizeWindow}></button>
-                <button type='button' className='expand' onClick={expandWindow}></button>
-                <button type='button' className='close' onClick={closeWindow}></button>
-            </div>
+            {props.showWindowMenu && <div className='title-bar-menu'>
+                    <button type='button'> <span>F</span>ile</button>
+                    <button type='button'> <span>E</span>dit</button>
+                    <button type='button'> <span>V</span>iew</button>
+                    <button type='button'> F<span>a</span>vorite</button>
+                    <button type='button'> <span>T</span>ools</button>
+                    <button type='button'> <span>H</span>elp</button>
+            </div>}
         </div>
     )
 }
--- src/Views/Window.tsx
+++ src/Views/Window.tsx
@@ -4,14 +4,14 @@ import { WindowTitleBar } from "../Components/WindowTitleBar"
 
 export const Window = (): React.ReactNode => {
     const onClick = (app: TaskbarData) => {
-        appService.activeAppId = app.id;
+        appService.addActiveApp(app);
     }
 
     return (
         appService.activeApps.map(app => {
             return (
             app.windowApp && <div key={`${app.id} window`} className={`window ${app.id} ${app.id === appService.activeAppId ? 'active': ''}`} onClick={() => onClick(app)} >
-                <WindowTitleBar {...app} />
+                <WindowTitleBar {...app}/>
                 <div className="window-body">
                     {app.windowApp}
                 </div>
